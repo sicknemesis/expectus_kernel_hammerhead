fa2f0ecc85d27720c9fb194837e2af50a4cdd3d4
Sicknemesis <hulluhalonen10@gmail.com>
2015-07-16 17:56:28 +0300

    Revert "UNDERCLOCK: initial commit, UC to 97mhz"
    This reverts commit e4d69532bdb97b89f0aaa05c52966c4728a56ef3.

--- arch/arm/mach-msm/acpuclock-8974.c
+++ arch/arm/mach-msm/acpuclock-8974.c
@@ -699,11 +699,6 @@ static struct acpu_level acpu_freq_tbl_2p2g_pvs6[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_2p3g_pvs0[] __initdata = {
-#ifdef CONFIG_CPU_UNDERCLOCK
-	{ 1, {   96000, HFPLL, 2,  10 },  L2(0),  800000,   1 },
-	{ 1, {  192000, HFPLL, 2,  20 },  L2(0),  800000,   1 },
-	{ 1, {  249600, HFPLL, 2,  26 },  L2(0),  800000,   1 },
-#endif
 	{ 1, {  300000, PLL_0, 0,   0 },  L2(0),  800000,  72 },
 	{ 0, {  345600, HFPLL, 2,  36 },  L2(1),  800000,  83 },
 	{ 1, {  422400, HFPLL, 2,  44 },  L2(2),  800000, 101 },
@@ -743,11 +738,6 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs0[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_2p3g_pvs1[] __initdata = {
-#ifdef CONFIG_CPU_UNDERCLOCK
-	{ 1, {   96000, HFPLL, 2,  10 },  L2(0),  800000,   1 },
-	{ 1, {  192000, HFPLL, 2,  20 },  L2(0),  800000,   1 },
-	{ 1, {  249600, HFPLL, 2,  26 },  L2(0),  800000,   1 },
-#endif
 	{ 1, {  300000, PLL_0, 0,   0 },  L2(0),  800000,  72 },
 	{ 0, {  345600, HFPLL, 2,  36 },  L2(1),  800000,  83 },
 	{ 1, {  422400, HFPLL, 2,  44 },  L2(2),  800000, 101 },
@@ -787,11 +777,6 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs1[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_2p3g_pvs2[] __initdata = {
-#ifdef CONFIG_CPU_UNDERCLOCK
-	{ 1, {   96000, HFPLL, 2,  10 },  L2(0),  800000,   1 },
-	{ 1, {  192000, HFPLL, 2,  20 },  L2(0),  800000,   1 },
-	{ 1, {  249600, HFPLL, 2,  26 },  L2(0),  800000,   1 },
-#endif
 	{ 1, {  300000, PLL_0, 0,   0 },  L2(0),  775000,  72 },
 	{ 0, {  345600, HFPLL, 2,  36 },  L2(1),  775000,  83 },
 	{ 1, {  422400, HFPLL, 2,  44 },  L2(2),  775000, 101 },
@@ -831,11 +816,6 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs2[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_2p3g_pvs3[] __initdata = {
-#ifdef CONFIG_CPU_UNDERCLOCK
-	{ 1, {   96000, HFPLL, 2,  10 },  L2(0),  800000,   1 },
-	{ 1, {  192000, HFPLL, 2,  20 },  L2(0),  800000,   1 },
-	{ 1, {  249600, HFPLL, 2,  26 },  L2(0),  800000,   1 },
-#endif
 	{ 1, {  300000, PLL_0, 0,   0 },  L2(0),  775000,  72 },
 	{ 0, {  345600, HFPLL, 2,  36 },  L2(1),  775000,  83 },
 	{ 1, {  422400, HFPLL, 2,  44 },  L2(2),  775000, 101 },
@@ -875,11 +855,6 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs3[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_2p3g_pvs4[] __initdata = {
-#ifdef CONFIG_CPU_UNDERCLOCK
-	{ 1, {   96000, HFPLL, 2,  10 },  L2(0),  800000,   1 },
-	{ 1, {  192000, HFPLL, 2,  20 },  L2(0),  800000,   1 },
-	{ 1, {  249600, HFPLL, 2,  26 },  L2(0),  800000,   1 },
-#endif
 	{ 1, {  300000, PLL_0, 0,   0 },  L2(0),  775000,  72 },
 	{ 0, {  345600, HFPLL, 2,  36 },  L2(1),  775000,  83 },
 	{ 1, {  422400, HFPLL, 2,  44 },  L2(2),  775000, 101 },
@@ -919,11 +894,6 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs4[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_2p3g_pvs5[] __initdata = {
-#ifdef CONFIG_CPU_UNDERCLOCK
-	{ 1, {   96000, HFPLL, 2,  10 },  L2(0),  800000,   1 },
-	{ 1, {  192000, HFPLL, 2,  20 },  L2(0),  800000,   1 },
-	{ 1, {  249600, HFPLL, 2,  26 },  L2(0),  800000,   1 },
-#endif
 	{ 1, {  300000, PLL_0, 0,   0 },  L2(0),  750000,  72 },
 	{ 0, {  345600, HFPLL, 2,  36 },  L2(1),  750000,  83 },
 	{ 1, {  422400, HFPLL, 2,  44 },  L2(2),  750000, 101 },
@@ -963,11 +933,6 @@ static struct acpu_level acpu_freq_tbl_2p3g_pvs5[] __initdata = {
 };
 
 static struct acpu_level acpu_freq_tbl_2p3g_pvs6[] __initdata = {
-#ifdef CONFIG_CPU_UNDERCLOCK
-	{ 1, {   96000, HFPLL, 2,  10 },  L2(0),  800000,   1 },
-	{ 1, {  192000, HFPLL, 2,  20 },  L2(0),  800000,   1 },
-	{ 1, {  249600, HFPLL, 2,  26 },  L2(0),  800000,   1 },
-#endif
 	{ 1, {  300000, PLL_0, 0,   0 },  L2(0),  750000,  72 },
 	{ 0, {  345600, HFPLL, 2,  36 },  L2(1),  750000,  83 },
 	{ 1, {  422400, HFPLL, 2,  44 },  L2(2),  750000, 101 },
